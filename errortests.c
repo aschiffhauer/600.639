f11a5cdc117d4992efd7f19542bbe6e7637df421
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2015-12-02 14:35:37 -0500

    test code for error correction (nyi)

--- errortests.c
+++ errortests.c
@@ -10,7 +10,7 @@
 #include "minsketch.h"
 #include "tests.h"
 
-#define KMER_SIZE 50
+#define KMER_SIZE 10
 #define FASTQ_FILE "reads.fastq"
 
 #define USING(x,y,z) histogram *h = histogram_new(x, y); ASSERT(h != NULL); z; histogram_free(h);
@@ -19,7 +19,21 @@
 #define FOR_EACH(x, y) fastq_for_each_kmer(FASTQ_FILE, KMER_SIZE, x, y);
 
 TEST(error_test1(), {
-
+	USING(MINSKETCH, minsketch_new(100, 100), {
+		READ();
+		int min = ~0U >> 1;
+		char min_kmer[KMER_SIZE + 1];
+		FOR_EACH(kmer, {
+			int count = COUNT(kmer);
+			if (count < min) {
+				PRINT("%s", kmer);
+				min = count;
+				strcpy(min_kmer, kmer);
+			}
+		});
+		PRINT("BAAGTTGAGT: %d", COUNT("BAAGTTGAGT"));
+		PRINT("%s: %d", min_kmer, min);
+	});
 })
 
 TEST(error_test(), {
