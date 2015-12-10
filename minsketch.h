444fb826ce13012fe263d93579c5bf2772b7d21c
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2015-12-10 15:15:37 -0500

    documented minsketch

--- minsketch.h
+++ minsketch.h
@@ -4,16 +4,21 @@
 #include <stdbool.h>
 
 typedef struct {
-	int w;
-	int d;
-	int **rows;
-	unsigned int *hashes;
+	int w;                 // width
+	int d;                 // height (number of hash functions)
+	int **rows;            // matrix
+	unsigned int *hashes;  // hash functions
 } minsketch;
 
+// Create a new bloomfilter with m bits and k hash functions
 minsketch* minsketch_new(int w, int d);
+// Adds a string to a bloomfilter
 bool minsketch_add(minsketch *m, const char *str);
+// Queries whether a string appears in a bloomfilter
 int minsketch_get(minsketch *m, const char *str);
-void minsketch_free(minsketch *m);
+// Gets the load factor (%) of a bloomfilter
 float minsketch_load_factor(minsketch *m);
+// Frees all dynamic memory allocations associated with a bloomfilter (including itself)
+void minsketch_free(minsketch *m);
 
 #endif
