f74872bac7567f87cfee02c61d9c47185d323b8b
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2015-12-10 14:41:15 -0500

    documented hash

--- hash.h
+++ hash.h
@@ -1,14 +1,14 @@
 #ifndef _HASH_H
 #define _HASH_H
 
+#define BITS_PER_CHAR (2) // Number of bits used per nucleotide
+#define MAX_KMER_SIZE (((sizeof(unsigned int) * 8)/BITS_PER_CHAR)) // Recommended max kmer size (16)
 #define A 0x0  // 0b00
 #define C 0x1  // 0b01
 #define G 0x2  // 0b10
 #define T 0x3  // 0b11
 
-#define BITS_PER_CHAR (2)
-#define MAX_HASH_SIZE ((sizeof(unsigned int) * 8)/BITS_PER_CHAR) - 1
-
+// Hashes a kmer; h(x) = ((djb2(x) + binary(x)) ^ seed)
 unsigned int hash(const char *str, unsigned int seed);
 
 #endif
